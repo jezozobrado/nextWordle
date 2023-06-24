9d4a1d7f7c8af10fe88c46b4a4c501db3b2864b2
MarlonDeOcampo <jezozobrado@gmail.com>
2023-06-24 15:02:29 +0800

    Implement axios.

--- app/wordle/page.tsx
+++ app/wordle/page.tsx
@@ -1,7 +1,40 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+interface IWord {
+  day: string;
+  num: string;
+  answer: string;
+}
 
 const Wordle = () => {
-  return <div>Wordle</div>;
+  const [words, setWords] = useState<IWord[]>();
+  const [solution, setSolution] = useState<string>();
+
+  useEffect(() => {
+    if (!words) return;
+
+    const wordBank = words.map((word) => word.answer);
+    const randomWord = wordBank[Math.floor(Math.random() * words.length)];
+    setSolution(randomWord);
+  }, [words]);
+
+  useEffect(() => {
+    axios
+      .get("https://wordle-answers-solutions.p.rapidapi.com/answers", {
+        headers: {
+          "X-RapidAPI-Key":
+            "058baf8782mshc8bf1e4463e4258p15c530jsn7b095ab2d73c",
+          "X-RapidAPI-Host": "wordle-answers-solutions.p.rapidapi.com",
+        },
+      })
+      .then((res) => setWords(res.data.data))
+      .catch((err) => console.error(err));
+  }, []);
+
+  return <section>{solution}</section>;
 };
 
 export default Wordle;
