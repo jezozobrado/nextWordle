ad0f323a755f0f7563a900f0915265b06aebaab7
MarlonDeOcampo <jezozobrado@gmail.com>
2023-06-24 14:26:23 +0800

    Set up next.

---
+++ app/contexto/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Contexto = () => {
+  return <div>Contexto</div>;
+};
+
+export default Contexto;
---
+++ components/Nav.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import Link from "next/link";
+import Image from "next/image";
+// import { signIn, signOut, useSession, getPRoviders } from "next-auth/react";
+
+const Nav = () => {
+  const isUserLoggedIn = true;
+  return (
+    <nav className="flex justify-between items-start w-full mb-16 pt-6 px-32 gap-20">
+      <Link className="flex gap-2 justify-center" href="/">
+        {/* <Image src="/next.svg" alt="Logo" width={30} height={30} /> */}
+        <p>Wordletopia</p>
+      </Link>
+      <div className="flex gap-10 flex-grow font-semibold">
+        <Link href="/wordle">Wordle</Link>
+        <Link href="/four">Four</Link>
+        <Link href="/contexto">Contexto</Link>
+      </div>
+
+      <Link href="/" className="black_btn">
+        Sign Out
+      </Link>
+    </nav>
+  );
+};
+
+export default Nav;
