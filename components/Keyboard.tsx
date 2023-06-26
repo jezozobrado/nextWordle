bb4c870db1b438ac970e390130912c73d13ba9d7
MarlonDeOcampo <jezozobrado@gmail.com>
2023-06-26 17:52:03 +0800

    Style keyboard.

--- components/Keyboard.tsx
+++ components/Keyboard.tsx
@@ -1,20 +1,45 @@
 import KEYS from "@constants/keyboard";
+import { LuDelete } from "react-icons/lu";
 
 interface Props {
   colors: { [letter: string]: string };
 }
 const Keyboard = ({ colors }: Props) => {
   return (
-    <section className="flex w-[500px] flex-wrap gap-1">
-      {KEYS.map((letter, i) => (
-        <div
-          key={i}
-          className="border w-16 h-10 flex justify-center items-center rounded-md"
-          style={{ background: `${colors[letter]}` }}
-        >
-          {letter.toUpperCase()}
-        </div>
-      ))}
+    <section className="flex flex-col gap-1 mt-5">
+      <div className="flex w-[650px] justify-between gap-1">
+        {KEYS.slice(0, 10).map((letter, i) => (
+          <div
+            key={i}
+            className="border h-10 flex justify-center items-center rounded-md  flex-auto"
+            style={{ background: `${colors[letter]}` }}
+          >
+            {letter.toUpperCase()}
+          </div>
+        ))}
+      </div>
+      <div className="flex w-[650px] justify-between gap-1">
+        {KEYS.slice(10, 19).map((letter, i) => (
+          <div
+            key={i}
+            className="border  h-10 flex justify-center items-center rounded-md flex-auto"
+            style={{ background: `${colors[letter]}` }}
+          >
+            {letter.toUpperCase()}
+          </div>
+        ))}
+      </div>
+      <div className="flex w-[650px]  justify-between gap-1">
+        {["DEL", ...KEYS.slice(19), "ENTER"].map((letter, i) => (
+          <div
+            key={i}
+            className="border h-10 flex justify-center items-center rounded-md flex-auto"
+            style={{ background: `${colors[letter]}` }}
+          >
+            {letter === "DEL" ? <LuDelete size="30px" /> : letter.toUpperCase()}
+          </div>
+        ))}
+      </div>
     </section>
   );
 };
