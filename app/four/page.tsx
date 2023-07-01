527a4acffa33c3dfbb226a8b3a8fc359c63d8114
MarlonDeOcampo <jezozobrado@gmail.com>
2023-07-01 18:57:48 +0800

    Add category on top of words.

--- app/four/page.tsx
+++ app/four/page.tsx
@@ -2,69 +2,66 @@
 import React, { useEffect, useMemo, useState } from "react";
 
 const Four = () => {
-  const solution = useMemo(
-    () => [
-      ["full", "king", "twin", "queen"],
-      ["gnat", "gnome", "gnocchi", "gnaw"],
-      ["gnu", "knew", "new", "nu"],
-      ["garlic", "mirror", "crucifix", "sunlight"],
-    ],
-    []
-  );
+  const rawSolution = [
+    ["full", "king", "twin", "queen", "Bed sizes"],
+    ["gnat", "gnome", "gnocchi", "gnaw", "Words with silent G"],
+    ["gnu", "knew", "new", "nu", "Homophones"],
+    ["garlic", "mirror", "crucifix", "sunlight", "Anti-vampire"],
+  ];
 
   const colors = useMemo(
     () => ["lightcoral", "sandybrown", "mediumaquamarine", "lightblue"],
     []
   );
 
+  console.log(rawSolution);
+
+  const [solution, setSolution] = useState<string[][]>([]);
   const [shuffledSolution, setShuffledSolution] = useState<string[]>();
   const [guesses, setGuesses] = useState<string[]>([]);
   const [correctCount, setCorrectCount] = useState(0);
   const [correctGuesses, setCorrectGuesses] = useState<string[][]>([]);
-  console.log(correctGuesses);
 
   const handleSubmit = (guesses: string[], solution: string[][]) => {
-    console.log("clicked");
     const stringifiedSolution = solution.map((s) => s.join(""));
     const stringifiedGuess = guesses.sort().join("");
 
     if (stringifiedSolution.includes(stringifiedGuess)) {
-      console.log(guesses);
-      // for (let i = 0; i < 4; i++) {
-      //   const tile = document.getElementById(guesses[i]);
-      //   if (!tile) return;
-      //   tile.style.backgroundColor = colors[correctCount];
-      // }
       setShuffledSolution((old) => old?.filter((o) => !guesses.includes(o)));
       setCorrectGuesses((o) => [...o, guesses]);
+      setGuesses([]);
+
+      const container = document.getElementById("container");
+      const children = container && container.children;
+      if (!children) return;
+
+      for (let i = 0; i < children.length; i++) {
+        let child = children[i] as HTMLElement;
+        child.style.removeProperty("background-color");
+      }
+
+      setCorrectCount((o) => o + 1);
     }
   };
 
   useEffect(() => {
-    const container = document.getElementById("container");
-    const children = container && container.children;
-    if (!children) return;
-    console.log(container?.children);
-
-    for (let i = correctGuesses.flat(2).length; i < children.length; i++) {
-      let child = children[i] as HTMLElement;
-      child.style.removeProperty("background-color");
-    }
-
     for (let i = 0; i < 4; i++) {
-      const tile = document.getElementById(guesses[i]);
+      const tile = document.getElementById(String(i));
       if (!tile) return;
-      tile.style.backgroundColor = colors[correctCount];
+      tile.style.backgroundColor = colors[i];
     }
-
-    setGuesses([]);
-    setCorrectCount((o) => o + 1);
   }, [shuffledSolution]);
 
+  useEffect(() => setSolution(rawSolution), []);
+
   useEffect(() => {
-    solution.map((s) => s.sort());
+    solution.map((s) => {
+      s.pop();
+      s.sort();
+    });
+
     setShuffledSolution(shuffleArray(solution.flat(2)));
-  }, []);
+  }, [solution]);
 
   useEffect(() => {
     shuffleArray(solution.flat(2));
@@ -73,10 +70,20 @@ const Four = () => {
   return (
     <>
       <div
-        className="grid grid-cols-4 grid-rows-4 w-[500px] m-auto gap-2 h-[250px]"
+        className="grid grid-cols-4 grid-rows-4 w-[550px] m-auto gap-2 h-[300px]"
         id="container"
       >
-        {correctGuesses.flat(2).map((word, i) => (
+        {correctGuesses.flat(0).map((words, i) => (
+          <div
+            key={i}
+            id={String(i)}
+            className=" flex justify-center items-center content-center rounded-md col-span-4 flex-col"
+          >
+            <span className="font-bold">{rawSolution[i][4]}</span>
+            <span>{words.join(", ").toUpperCase()}</span>
+          </div>
+        ))}
+        {/* {correctGuesses.flat(2).map((word, i) => (
           <div
             key={i}
             id={word}
@@ -84,7 +91,7 @@ const Four = () => {
           >
             {word.toUpperCase()}
           </div>
-        ))}
+        ))} */}
         {shuffledSolution?.map((word, i) => (
           <div
             key={i}
@@ -99,11 +106,13 @@ const Four = () => {
 
               if (tile.style.backgroundColor === "lightgray") {
                 tile.style.removeProperty("background-color");
+
                 setGuesses((g) => g.filter((x) => x !== word));
                 return;
               }
 
               if (guesses?.length >= 4) return;
+
               tile.style.backgroundColor = "lightgray";
               setGuesses((g) => [...g, word]);
             }}
